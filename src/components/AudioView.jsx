f4c86c45c4c7ce9611d80e56670ec12864e56d10
dongdongZhang007 <zhangdy0601@outlook.com>
2022-10-08 12:45:23 +0800

    add audio api test

--- src/components/AudioView.jsx
+++ src/components/AudioView.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import {
-    // observable,
+    action,
+    observable,
     makeObservable,
-    // runInAction,
+    runInAction,
     // autorun,
 } from 'mobx';
 import { observer } from 'mobx-react';
+
+const size = 1024;
 
 // https://segmentfault.com/a/1190000018809821
 // https://github.com/pkjy/blog/issues/2
 // https://developer.mozilla.org/zh-CN/docs/Web/API/Web_Audio_API/Using_Web_Audio_API#%E9%9F%B3%E9%A2%91%E4%B8%8A%E4%B8%8B%E6%96%87
 // 二进制的音频文件流测试
 const AudioView = observer(class AudioView extends React.Component {
+    vol = 30;
+    canvasBox = null;
+    canvasDom = null;
+    canvasCtx = null;
+    boxWidth = 0;
+    boxHeight = 0;
+
     constructor(props) {
         super(props);
         makeObservable(this, {
+            vol: observable,
+            changeVol: action.bound,
+            visualizer: action.bound,
+            resize: action.bound,
+            createCanvas: action.bound,
+            draw: action.bound,
         });
     }
 
     componentDidMount() {
-        
+        console.log("componentDidMount");
+        this.createCanvas();
+        this.resize();
     }
 
     play = (arrayBuffer) => {
@@ -50,10 +68,19 @@ const AudioView = observer(class AudioView extends React.Component {
         // 去除文件头之后的音频数据
         const IsLittleEndian = true;
         // 创建AudioContext上下文
-        const AudioContext = window.AudioContext || window.webkitAudioContext,
-            audioCxt = new AudioContext();
+        const AudioContext = window.AudioContext || window.webkitAudioContext;
+        this.audioCxt = new AudioContext();
         // 创建一个 AudioBufferSourceNode 对象，使用 AudioContext 的工厂函数创建
-        const audioNode = audioCxt.createBufferSource();
+        this.audioNode = this.audioCxt.createBufferSource();
+
+        // 创建一个控制音量大小的节点对象
+        this.gainNode = this.audioCxt[this.audioCxt.createGain?"createGain":"createGainNode"]();
+        this.gainNode.connect(this.audioCxt.destination);
+
+        // 创建音频分析对象
+        this.analyserNode = this.audioCxt.createAnalyser();
+        this.analyserNode.connect(this.gainNode);
+        this.analyserNode.ffzSize = size * 2;
 
         // https://www.cnblogs.com/douzujun/p/10600793.html
         const dv = new DataView(arrayBuffer); 
@@ -129,7 +156,7 @@ const AudioView = observer(class AudioView extends React.Component {
         console.log(frameCount);
         // https://developer.mozilla.org/zh-CN/docs/Web/API/AudioBuffer
         // let audioBuffer = audioCxt.createBuffer(NumChannnels, frameCount, SampleRate);
-        let audioBuffer = audioCxt.createBuffer(NumChannnels, frameCount, SampleRate);
+        let audioBuffer = this.audioCxt.createBuffer(NumChannnels, frameCount, SampleRate);
 
         // https://blog.csdn.net/zhihu008/article/details/7854529
         // 16bit 单声道 8bit 单声道
@@ -138,14 +165,14 @@ const AudioView = observer(class AudioView extends React.Component {
             let j = 0, i = offset;
             while(i < dv.byteLength) {
                 if(BitsPerSample === 8) {
-                    nowBuffering[j++] = dv.getInt8(i)/64;
+                    nowBuffering[j++] = dv.getInt8(i) / 256;
                     i++;
                 } else if(BitsPerSample === 16) {
                     nowBuffering[j++] = dv.getInt16(i, IsLittleEndian)/65536;
                     i += 2;
                 }
             }
-            console.log(nowBuffering);
+            // console.log(nowBuffering);
         }
 
         // 16bit 双声道
@@ -163,11 +190,28 @@ const AudioView = observer(class AudioView extends React.Component {
             }
         }
 
-        audioNode.buffer = audioBuffer;
+        this.audioNode.buffer = audioBuffer;
         console.log("without header parse:", audioBuffer);
-        audioNode.connect(audioCxt.destination);
+        this.audioNode.connect(this.analyserNode);
         // 从 0s 开始播放
-        audioNode.start(0);
+        this.audioNode[this.audioNode.start?"start":"noteOn"](0);
+        this.visualizer();
+    }
+
+    visualizer() {
+        let arr = new Uint8Array(this.analyserNode.frequencyBinCount);
+        let requestAnimationFrame = window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame;
+
+        const v = () => {
+            this.analyserNode.getByteFrequencyData(arr);
+            this.draw(arr); // 绘制图形
+            // console.log(arr);
+            requestAnimationFrame(v);
+        };
+
+        requestAnimationFrame(v);
     }
 
     playMusic = (target) => {
@@ -220,6 +264,40 @@ const AudioView = observer(class AudioView extends React.Component {
         fileReader.readAsArrayBuffer(file);
     }
 
+    changeVol(val) {
+        this.gainNode.gain.value = val * val;
+    }
+
+    resize() {
+        this.boxHeight = this.canvasBox.clientHeight ? this.canvasBox.clientHeight : 0;
+        this.boxWidth = this.canvasBox.clientWidth ? this.canvasBox.clientWidth : 0;
+        this.canvasDom.height = this.boxHeight;
+        this.canvasDom.width = this.boxWidth;
+        // 创建渐变色
+        let line = this.canvasCtx.createLinearGradient(0, 0, 0, this.boxHeight);
+        line.addColorStop(0, "red");
+        line.addColorStop(0.5, "yellow");
+        line.addColorStop(1, "green");
+        this.canvasCtx.fillStyle = line; // 填充渐变色
+    }
+
+    createCanvas() {
+        this.canvasBox = document.getElementById('canvasBox');
+        this.canvasDom = document.createElement("canvas");
+        this.canvasBox.appendChild(this.canvasDom);
+        this.canvasCtx = this.canvasDom.getContext("2d");
+    }
+
+    draw(arr) {
+        this.canvasCtx.clearRect(0, 0, this.boxWidth, this.boxHeight);
+        let w = this.boxWidth / size;
+        // 绘制矩形
+        for(let i = 0; i < size; i++) {
+            let h = arr[i] / 256 * this.boxHeight;
+            this.canvasCtx.fillRect(w * i, this.boxHeight - h, w * 0.6, h);
+        }
+    }
+
     render() {
         return (
             <div>
@@ -228,10 +306,23 @@ const AudioView = observer(class AudioView extends React.Component {
                     // console.log(event);
                     this.playMusic(event.target)
                 }} />
-                <input type="file" onChange={(event) => {
-                    // console.log(event);
-                    this.playMusic1(event.target)
-                }} />
+                <div>
+                    <span>音量控制：</span>&nbsp;
+                    <input type="range" 
+                        min="0" 
+                        max="100"
+                        onChange={(evt) => {
+                            runInAction(()=> {
+                                this.vol = evt.target.value;
+                                this.changeVol(this.vol);
+                            });
+                        }} />
+                    <input type="file" onChange={(event) => {
+                        // console.log(event);
+                        this.playMusic1(event.target)
+                    }} />
+                </div>
+                <div className="canvas-box" id="canvasBox"></div>
                 <audio className="audio-node" autoPlay></audio>
             </div>
         )
